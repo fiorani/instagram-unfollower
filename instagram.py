266f107b4acd6e1ad9bc19fb2fafd7ba8f9c3a46
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-03-19 09:45:06 +0100

    update while condition

--- instagram.py
+++ instagram.py
@@ -26,21 +26,14 @@ num = WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.
 # vai ai follower
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[@href='/"+username+"/followers/']"))).click()
 follower_list = []
-count = 0
-while True:
-    try:
-        elements = WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, "//span/div")))
-        for element in elements:
-            count += 1
-            friend_username = element.text
-            if "Italiano" not in friend_username and friend_username not in follower_list:
-                follower_list.append(friend_username)
-        driver.execute_script("arguments[0].scrollIntoView(true);", elements[-1])
-        sleep(2)
-        if count > int(num[1].text):
-            break
-    except NoSuchElementException:
-        continue
+while len(follower_list) < int(num[1].text):
+    elements = WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, "//span/div")))
+    for element in elements:
+        friend_username = element.text
+        if "Italiano" not in friend_username and friend_username not in follower_list:
+            follower_list.append(friend_username)
+    driver.execute_script("arguments[0].scrollIntoView(true);", elements[-1])
+    sleep(2)
 WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, "//button[contains(@class, '_abl-')]")))[1].click()
 print("follower_list")
 print(follower_list)
@@ -48,19 +41,17 @@ print(follower_list)
 # vai ai following e smette di seguire chi non ti segue
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[@href='/"+username+"/following/']"))).click()
 count = 0
-while True:
-    try:
-        elements = WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, "//div[contains(@class,'x9f619 x1n2onr6 x1ja2u2z x78zum5 x2lah0s x1qughib x6s0dn4 xozqiw3 x1q0g3np')]")))
-        for element in elements:
+while count < int(num[2].text):
+    elements = WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, "//div[contains(@class,'x9f619 x1n2onr6 x1ja2u2z x78zum5 x2lah0s x1qughib x6s0dn4 xozqiw3 x1q0g3np')]")))
+    for element in elements:
+        friend_username = element.find_element(By.XPATH,".//span/div").text
+        if "Italiano" not in friend_username and friend_username not in follower_list:
             count += 1
-            friend_username = element.find_element(By.XPATH,".//span/div").text
-            if "Italiano" not in friend_username and friend_username not in follower_list:
-                element.find_element(By.XPATH,".//button[contains(@class, '_acan')]").click()
-                WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Non seguire più']"))).click()
-                sleep(2)
-        driver.execute_script("arguments[0].scrollIntoView(true);", driver.find_elements(By.XPATH,"//span/div")[-1])
-        sleep(2)
-        if count > int(num[2].text):
-            break
-    except NoSuchElementException:
-        continue
+            element.find_element(By.XPATH,".//button[contains(@class, '_acan')]").click()
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Non seguire più']"))).click()
+            sleep(2)
+    driver.execute_script("arguments[0].scrollIntoView(true);", driver.find_elements(By.XPATH,"//span/div")[-1])
+    sleep(2)
+
+WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, "//button[contains(@class, '_abl-')]")))[1].click()
+sleep(20)
